5aadf683c6a757dfc8e4a8b04216f9bc400044a8
Sasha <sashapysmenny@gmail.com>
2021-03-23 13:12:50 +0200

    init

---
+++ delegat_1/delegat_1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace delegat_1
+{
+
+    static class Program
+    {
+        delegate double Delegat(double yourNumber);
+        static double AddNumberToDefaultValue(double yourNumber) => yourNumber + 3.5;
+        static double SquareRootOfNumberModule(double yourNumber) => Math.Sqrt(Math.Abs(yourNumber));
+        static double NumberInThirdDegree(double yourNumber) => Math.Pow(yourNumber, 3);
+        
+
+        static void Main(string[] args)
+        {
+            Console.Write("Виводьте рядки послідовно один за одним\n" +
+                "Поки вони матимуть вигляд 0х, чи 1х, чи 2х\n" +
+                "(тобто цифра від 0 до 2, а після неї запис конкретного дійсного числа),\n" +
+                "програма обчислюватиме одну з трьох функцій\n" +
+                "\t0 - sqrt(abs(x))\n" +
+                "\t1 - x^3(інакше кажучи х*х*х)\n" +
+                "\t2 - х + 3,5\n" +
+                "згідно цифри на початку і виводитиме результат.\n" +
+                "\nЯкщо вхідний рядок не задовольняє цей формат, програма завершить роботу.\n");
+
+            Delegat[] delegat = new Delegat[3] { SquareRootOfNumberModule, NumberInThirdDegree, AddNumberToDefaultValue };
+            while (true)
+            {
+                try
+                {
+                    string[] data = Console.ReadLine().Split(' ');
+                    int operation = int.Parse(data[0]);
+                    double num = int.Parse(data[1]);
+                    Console.WriteLine(delegat[operation](num));
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Введені дані не задовольняють потрібний формат. Натисніть будь-яку клавішу для виходу.");
+                    Console.ReadKey();
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ delegats_2/delegats_2/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace delegats_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.Opacity != 1)
+                this.Opacity = 1;
+            else
+                this.Opacity = 0.5;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (this.BackColor != Color.White)
+                this.BackColor = Color.White;
+            else
+                this.BackColor = Color.Coral;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello Home!\n" +
+                "World заблокований з міркувань карантину");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Я супермегакнопка\n" +
+                "і цього мене не позбавиш!");
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+                this.button4.Click += new System.EventHandler(this.button1_Click);
+            else
+                this.button4.Click -= new System.EventHandler(this.button1_Click);
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+                this.button4.Click += new System.EventHandler(this.button2_Click);
+            else
+                this.button4.Click -= new System.EventHandler(this.button2_Click);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked)
+                this.button4.Click += new System.EventHandler(this.button3_Click);
+            else
+                this.button4.Click -= new System.EventHandler(this.button3_Click);
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked)
+                this.button4.Click += new System.EventHandler(this.button3_Click);
+            else
+                this.button4.Click -= new System.EventHandler(this.button3_Click);
+        }
+    }
+}
